a1364dc76f04b077a4da343899c21976b3c44906
MARTIKRISO <mshkodrov@gmail.com>
2022-09-27 16:56:14 +0300

    Added User table

---
+++ DatabaseLayer/FlightManagerContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLayer
+{
+    public class FlightManagerContext : DbContext
+    {
+        public FlightManagerContext() : base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost/SQLEXPRESS;Database=FlightManagerDB;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+       // public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ DatabaseLayer/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLayer
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string FName { get; set; }
+        [Required]
+        public string LName { get; set; }
+        [Required]
+        public string EGN { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Role { get; set; }
+        private User()
+        {
+
+        }
+        public User(string username, string password, string email, string fname, string lname, string egn, string address, string phonenumber, string role = "staff")
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            FName = fname;
+            LName = lname;
+            EGN = egn;
+            Address = address;
+            PhoneNumber = phonenumber;
+            Role = role;
+
+        }
+    }
+}
